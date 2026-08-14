58742d3afc4d69cf9842a05ab3979259dad1e11f
Bhargavbaluni <balunibhargav@gmail.com>
2026-08-14 11:20:47 +0530

    fix cors for vercel admin frontend

--- src/Pages/Nissan_Register/RegisterPage2/RegisterPage2.jsx
+++ src/Pages/Nissan_Register/RegisterPage2/RegisterPage2.jsx
@@ -10,7 +10,6 @@ const RegisterPage2 = ({
   players,
 }) => {
   console.log(events);
-  console.log("TOURNAMENTS:", tournaments);
   const [errors, setErrors] = useState({});
   const [event2List, setEvent2List] = useState(events);
   const [isEvent2Selected, setIsEvent2Selected] = useState(false);
