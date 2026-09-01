f9517ebc0675abbfda6cc05bd636df99b831ae6b
Bhargavbaluni <balunibhargav@gmail.com>
2026-09-01 10:57:19 +0530

    fix cors for vercel admin frontend

--- src/redux/user.slice.js
+++ src/redux/user.slice.js
@@ -1,18 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const savedUser = localStorage.getItem("user");
+
 const userSlice = createSlice({
   name: "user",
+
   initialState: {
-    user: {},
+    user: savedUser ? JSON.parse(savedUser) : {},
   },
+
   reducers: {
-    //actions
     setUser: (state, action) => {
       state.user = action.payload;
+
+      // Save user data to localStorage after login
+      localStorage.setItem("user", JSON.stringify(action.payload));
     },
   },
 });
 
-export const { setUser: setUser } = userSlice.actions;
+export const { setUser } = userSlice.actions;
 
 export default userSlice.reducer;
