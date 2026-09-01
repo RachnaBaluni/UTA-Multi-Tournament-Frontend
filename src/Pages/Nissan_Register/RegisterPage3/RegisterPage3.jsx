8ea13071a128ad8a38ae8ce533af56cb5d260665
Bhargavbaluni <balunibhargav@gmail.com>
2026-09-01 10:36:34 +0530

    fix cors for vercel admin frontend

--- src/Pages/Nissan_Register/RegisterPage3/RegisterPage3.jsx
+++ src/Pages/Nissan_Register/RegisterPage3/RegisterPage3.jsx
@@ -100,7 +100,7 @@ const RegisterPage3 = ({ formData }) => {
               type="button"
               className={styles.button}
               onClick={() => {
-                navigate(
+                window.location.assign(
                   `/tournaments/register?tournamentId=${formData.tournamentId}`,
                 );
               }}
